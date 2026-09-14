d6d59bae6c8582e59c23301a77e3b9dc5a243ba9
rhyden <rhyden@campbellsci.com>
2026-09-14 12:19:49 -0600

    modified the config for vercel.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 import process from 'node:process'
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: '2025-07-15',
+//   devtools: { enabled: process.env.NODE_ENV !== 'production' },
+//   nitro: { preset: 'node-server' }
+// })
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
-  nitro: { preset: 'node-server' }
+  nitro: { preset: 'vercel' }
 })
